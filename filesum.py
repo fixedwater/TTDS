a6c85c9d1ea49065c5fd7ec202df7fda6fac09e6
Zz <55668772+Deverend@users.noreply.github.com>
2020-02-16 14:50:08 +0000

    Add files via upload

--- filesum.py
+++ filesum.py
@@ -8,11 +8,20 @@ for tag in ['Business','entertainment','health','politics','Sci_Technology','spo
         root_list.append(root)
         
     f=open('Articles.xml','w',encoding='UTF-8')
+    f.writelines('<?xml version="1.0" ?>\n')
+    f.writelines('<DATA>\n')
     for path in root_list[1:]:
         filepath = path+'\\Articles.xml'
         with open(filepath,'r',encoding='UTF-8') as fr:
             for line in fr:
+                if line == '<?xml version="1.0" ?>\n':
+                    continue
+                if line == '<DATA>\n':
+                    continue
+                if line == '</DATA>\n':
+                    continue
                 f.writelines(line)
+    f.writelines('</DATA>\n')
     f.close()
 
 
@@ -20,9 +29,18 @@ os.chdir('F:\\Mytools\\workspace\\TTDS-master\\catagery-bbc\\')
 file_chdir = os.getcwd()
 
 f=open('Articles.xml','w',encoding='UTF-8')
+f.writelines('<?xml version="1.0" ?>\n')
+f.writelines('<DATA>\n')
 for tag in ['Business','entertainment','health','politics','Sci_Technology','sport']:
     filepath = file_chdir+'\\'+tag+'\\Articles.xml'
     with open(filepath,'r',encoding='UTF-8') as fr:
             for line in fr:
+                if line == '<?xml version="1.0" ?>\n':
+                    continue
+                if line == '<DATA>\n':
+                    continue
+                if line == '</DATA>\n':
+                    continue
                 f.writelines(line)
+f.writelines('</DATA>\n')
 f.close()
